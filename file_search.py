b91503f01fd0312bc8c0eec77229c16641384925
gusgus98 <fernanda-mtz24@hotmail.com>
2025-03-20 10:28:01 -0400

    Update file_search.py

--- file_search.py
+++ file_search.py
@@ -4,8 +4,10 @@ from tkinter import Tk, filedialog
 def select_file():
     root = Tk()
     root.withdraw()  # Hide the root window
-    file_path = filedialog.askopenfilename(title="Select file", filetypes=[("files", "*.txt")])
+    file_path = filedialog.askopenfilename(title="Select file", filetypes=[("files", "*")])  # Open file dialog to select a file
+    print(file_path)  # Print the selected file path
     return file_path
+
 
 
 if __name__ == '__main__':
