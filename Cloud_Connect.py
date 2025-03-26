f0e7fef0cd09574582488da690a628e87e5c6228
Andrew Seibert <andrew.seibert@temple.edu>
2025-03-26 14:25:43 -0400

    successful test of sending a file to s3 bucket - but requires hard code of access and secret key

--- Cloud_Connect.py
+++ Cloud_Connect.py
@@ -3,8 +3,12 @@ import logging
 from botocore.exceptions import ClientError
 from tkinter import *
 
+# change 'YOUR_ACCESS_KEY_ID' and 'YOUR_SECRET_ACCESS_KEY' for each user
+AWS_ACCESS_KEY_ID = 'YOUR_ACCESS_KEY_ID'
+AWS_SECRET_ACCESS_KEY = 'YOUR_SECRET_ACCESS_KEY'
+
 def upload(file_path, bucket_name, file_key=None):
-    bucket_client = boto3.client("s3")
+    bucket_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
     # file_key = what will show in s3
     if file_key is None:
@@ -22,5 +26,6 @@ def upload(file_path, bucket_name, file_key=None):
     return True
 
 if __name__ == '__main__':
-    #upload("C:\Users\absei\OneDrive\Documents\cs3296\Final Project\s3FileTest1.txt", "templecloudlockbucket")
+    # testing file upload 
+    upload(r"C:\Users\absei\OneDrive\Documents\cs3296\Final Project\s3FileTest1.txt", "templecloudlockbucket")
     print("Cloud_Connect main")
