afa42dfa0ec46b86ab607dfa6b590fadc08a20e7
Timofei Gomziakov <tuk36968@temple.edu>
2025-04-04 12:37:54 -0400

    Fixed some GUI bugs; back button added; directory added.

--- GUI/UserInterface.py
+++ GUI/UserInterface.py
@@ -4,8 +4,10 @@ from tkinter import *
 #* imports everything
 from PIL import ImageTk, Image
 from BackEnd.Microsoft_Auth import authenticate_acct, create_one_time_password, verify_user_code
+import BackEnd.file_search as fs
 
 global_image_list = [] # global image list to avoid the garbage collection 
+current_frame = None
 
 def changeView(root : tk.Frame, view):
     for child in root.winfo_children():
@@ -16,6 +18,8 @@ class QrView:
     "Accepts: callback"
     # TODO: Createa a destructor to cleanup global image list
     def __init__(self, parent : tk.Frame, **kwargs):
+        self.name = "qrview"
+        current_frame = self.name
         self.callback = kwargs.get("callback", None)
         self.root_frame = parent
         text_variable = StringVar()
@@ -53,12 +57,24 @@ class TokenView:
         self.root_frame = parent
         self.callback = kwargs.get("callback", None)
 
+        self.name = "tokenview"
+        current_frame = self.name
+
         # Configure rows' and columns' weights
         for x in range(0,5):
             parent.rowconfigure(x,weight=1)
             parent.columnconfigure(x, weight=1)
 
-        tk.Label(parent, text="2FA Authentication", font=("TkDefaultFont", 18)).grid(column=2, sticky="ns")
+        info_frame = tk.Frame(parent)
+        info_frame.grid(row=0, columnspan=5, sticky="ew")
+
+        for x in range(0,5):
+            info_frame.rowconfigure(x,weight=1)
+            info_frame.columnconfigure(x, weight=1)
+
+        tk.Button(info_frame, text="Back").grid(row=0, column=0, sticky="nw")
+        tk.Label(info_frame, text="2FA Authentication", font=("TkDefaultFont", 18)).grid(row=0, column=1, sticky="ns")
+
 
         # Create image using PIL (required for .jpg files)
         image = Image.open("./Images/2fa.jpg")
@@ -66,7 +82,7 @@ class TokenView:
         image= ImageTk.PhotoImage(image)
         global_image_list.append(image)
 
-        tk.Label(parent).grid(row=1, column=2, columnspan=1, sticky="n")
+        tk.Label(parent, image=image).grid(row=1, column=0, columnspan=5)
 
         # Create another frame to hold the information
         loginFrame = tk.Frame(parent)
@@ -78,22 +94,27 @@ class TokenView:
             loginFrame.columnconfigure(x, weight=1)
 
         tk.Label(loginFrame, text="2FA - Token", font=("TkDefaultFont", 12)).grid(column=0, row=1)
-        tk.Entry(loginFrame, textvariable=self.qrCode, font=("TkDefaultFont", 12)).grid(column=1, row=1)
 
-        tk.Button(loginFrame, text="Log In", font=("TkDefaultFont", 12), command= lambda : self.button_clicked_verify()).grid(column=0, row=2, columnspan=2)
+        entry = tk.Entry(loginFrame, textvariable=self.qrCode, font=("TkDefaultFont", 12))
+        entry.grid(column=1, row=1)
+        entry.bind("<Return>", self.button_clicked_verify)
 
-    def button_clicked_verify(self):
+        tk.Button(loginFrame, text="Log In", font=("TkDefaultFont", 12), command= lambda : self.button_clicked_verify(None)).grid(column=0, row=2, columnspan=2)
+
+    def button_clicked_verify(self, event):
         str_qrcode = self.qrCode.get()
         success_or_not = verify_user_code(str_qrcode, create_one_time_password())
 
         if(success_or_not): #take the code inout by user, compare it to TOTP created
             print("Correct code!")
-            #changeView(self.root_frame, HomeView)
+            # changeView(self.root_frame, HomeView)
 
 class FileEncryption:
     "Accepts: home_callback, encryption_callback"
     def __init__(self, parent, **kwargs):
         self.row_count = 0
+        self.name = "fileencryption"
+        current_frame = self.name
 
         for x in range(0,5):
             parent.rowconfigure(x,weight=1)
@@ -116,13 +137,13 @@ class FileEncryption:
         self.create_buttons()
 
     def create_title_label(self):
-        title_lbl = tk.Label(self.root, text='File Encryption System', font=("Helvetica", 16, "bold"), fg="#333", bg="#f0f0f0")
+        title_lbl = tk.Label(self.root, text='File Encryption System', font=("Helvetica", 16, "bold"), fg="#333")
         title_lbl.grid(row=0, column=0, columnspan=5, sticky="news")
     
     def create_file_path_entry(self):
-        file_path_lbl = tk.Label(self.holder, text="File Path:", font=("Helvetica", 12), bg="#f0f0f0")
+        file_path_lbl = tk.Label(self.holder, text="File Path:", font=("Helvetica", 12))
         file_path_entry = tk.Entry(self.holder, font=("Helvetica", 12), bd=2, relief="solid")
-        file_path_browse = tk.Button(self.holder, text="Browse", command=lambda : browse())
+        file_path_browse = tk.Button(self.holder, text="Browse", font=("Helvetica", 12), command=lambda : browse())
 
         file_path_lbl.grid(row=self.row_count, column=0, sticky="e")
         file_path_entry.grid(row=self.row_count, column=1, sticky="ew")
@@ -130,11 +151,12 @@ class FileEncryption:
 
         self.row_count += 1
         def browse():
-            print("Browse")
+            file = fs.select_file()
+            print(file)
             return
 
     def create_file_name_entry(self):
-        file_name_lbl = tk.Label(self.holder, text="File Name:", font=("Helvetica", 12), bg="#f0f0f0")
+        file_name_lbl = tk.Label(self.holder, text="File Name:", font=("Helvetica", 12))
         file_name_entry = tk.Label(self.holder, font=("Helvetica", 12), bd=2, relief="solid")
 
         file_name_lbl.grid(row=self.row_count, column=0, sticky="e")
@@ -143,7 +165,7 @@ class FileEncryption:
         self.row_count += 1
 
     def create_file_key_entry(self):
-        file_key_lbl = tk.Label(self.holder, text="File Key:", font=("Helvetica", 12), bg="#f0f0f0")
+        file_key_lbl = tk.Label(self.holder, text="File Key:", font=("Helvetica", 12))
         file_key_entry = tk.Entry(self.holder, font=("Helvetica", 12), bd=2, relief="solid")
 
         file_key_lbl.grid(row=self.row_count, column=0)
@@ -152,7 +174,7 @@ class FileEncryption:
         self.row_count += 1
 
     def create_file_desc_entry(self):
-        file_desc_lbl = tk.Label(self.holder, text="File Type:", font=("Helvetica", 12), bg="#f0f0f0")
+        file_desc_lbl = tk.Label(self.holder, text="File Type:", font=("Helvetica", 12))
         self.file_desc_entry = tk.Label(self.holder, font=("Helvetica", 12), bd=2, relief="solid")
 
         file_desc_lbl.grid(row=self.row_count, column=0, sticky="e")
@@ -181,16 +203,16 @@ class FileEncryption:
         print("Home button clicked")
 
 def create_directory(root, row_count):
-        #TODO: Get currently active menu
-
         holder = tk.Frame()
         holder.grid(row=row_count, columnspan=5, sticky="ews")
+
         for x in range(2):
             holder.columnconfigure(x, weight=1)
-        tk.Button(holder, text="Auth", fg="white", bg="#28a745", font=("Helvetica", 12, "bold"), relief="raised", bd=2).grid(column=0, row=0, sticky="ew")
-        tk.Button(holder, text="File Upload", fg="white", bg="#28a745", font=("Helvetica", 12, "bold"), relief="raised", bd=2).grid(column=1, row=0, sticky="we")
-        tk.Button(holder, text="QR Generator", fg="white", bg="#28a745", font=("Helvetica", 12, "bold"), relief="raised", bd=2).grid(column=0, row=1, sticky="ew")
-        tk.Button(holder, text="Encrypt/Decrypt", fg="white", bg="#28a745", font=("Helvetica", 12, "bold"), relief="raised", bd=2, command= lambda : changeView(root, QrView)).grid(column=1, row=1, sticky="we")
+
+        tk.Button(holder, text="Upload/Download", fg="white", bg=f"{"#28a745" if current_frame == "" else "#28a745"}", font=("Helvetica", 12, "bold"), 
+                  relief="raised", bd=2).grid(column=0, row=0, sticky="we")
+        tk.Button(holder, text="Encrypt/Decrypt", fg="white", bg="#28a745", font=("Helvetica", 12, "bold"), 
+                  relief="raised", bd=2, command= lambda : changeView(root, QrView)).grid(row=0, column=1, sticky="we")
         
         
         row_count += 1
