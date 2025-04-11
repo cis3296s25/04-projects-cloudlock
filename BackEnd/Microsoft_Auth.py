a65fd2732335970503e71e20675374a22c970641
916046794 <tuo74327@temple.edu>
2025-04-10 22:00:26 -0400

    Finished backend acct choices for auth

--- BackEnd/Microsoft_Auth.py
+++ BackEnd/Microsoft_Auth.py
@@ -12,7 +12,7 @@ import json
 
 #secret_key = pyotp.random_base32()
 #secret_key = base64.b32encode(b"6THUVVE5BKJG2DOXTSMYDD62BS2EWW2I")
-SECRET_KEY = "6THUVVE5BKJG2DOXTSMYDD62BS2EWW2I"
+#SECRET_KEY = "6THUVVE5BKJG2DOXTSMYDD62BS2EWW2I"
 #I think secret_key needs to remain consistent for the userInterface to call these methods separately
 #but with the same value to link them together (secret_key)
 
@@ -20,37 +20,6 @@ SECRET_KEY = "6THUVVE5BKJG2DOXTSMYDD62BS2EWW2I"
 BASE_DIR = os.path.dirname(__file__)
 QR_DIR = os.path.join(BASE_DIR, "AuthenticationCredentials")
 AUTH_PATH = os.path.join(QR_DIR, "authentication_value.json")
-
-def get_secret_key(name_of_user):
-    randomized_code = pyotp.random_base32()
-
-    if os.path.exists(QR_DIR):
-        print("Directory 'AuthenticationCredentials' already established.")
-    else:
-        os.makedirs(QR_DIR)
-        print("Directory 'AuthenticationCredentials' created.")
-
-    # ---------------------------------------------------------------------------------------------------------
-
-    if os.path.exists(AUTH_PATH):
-        #if the json file exists, open the file with reading permissions and load the information. Based on the
-        #username the user inputs, it will traverse the dictionary/Map to find the matching key
-        print("QR authentication already established")
-
-        with open(AUTH_PATH, 'r') as qrFile:
-            data = json.load(qrFile)
-            print("Qr verification link: ", data[name_of_user])
-
-            return data[name_of_user]
-    else:
-        data = {name_of_user: randomized_code}
-
-        with open(AUTH_PATH, "w") as qrFile:
-            json.dump(data, qrFile, indent=4)
-            print("Qr verification link created")
-
-            return randomized_code
-
 
 def get_secret_key(name_of_user):
     randomized_code = pyotp.random_base32()
@@ -70,7 +39,7 @@ def get_secret_key(name_of_user):
 
     with open(AUTH_PATH, 'r') as qrFile:
         data = json.load(qrFile)
-        print("Qr verification link: ", data[name_of_user])
+        print("Qr verification link: ", data)
 
         if data.get(name_of_user, 0) != 0:
             #if it exists
@@ -86,19 +55,29 @@ def get_secret_key(name_of_user):
         return randomized_code
 
 
-def authenticate_acct(name_of_user):
-    authentication_link = pyotp.totp.TOTP(SECRET_KEY).provisioning_uri(name_of_user,"CloudLockTeam")
+def authenticate_acct(name_of_user, secret_key):
+    authentication_link = pyotp.totp.TOTP(secret_key).provisioning_uri(name_of_user,"CloudLockTeam")
     #name_of_user is their google authenticator username and CloudLockTeam is the name of the issuer
     #creates the account for the user that is connected to the secret_key
+    print(authentication_link)
 
-    qrcode.make(authentication_link).save("./Images/qr-code.png")
+    qrcode.make(authentication_link).save("../Images/qr-code.png")
 
-def create_one_time_password():
+def create_one_time_password(secret_key):
     #creates the link for the time-based password based on the secret-key connected to the user
-    qrcode_linked = pyotp.TOTP(SECRET_KEY)
+    qrcode_linked = pyotp.TOTP(secret_key)
 
     return qrcode_linked
 
 def verify_user_code(user_code, correct_otp):
     #verifies the code the user connects with the correct authentication code from the link
     return correct_otp.verify(user_code)
+
+if __name__ == "__main__":
+    secret_key = get_secret_key("stellar")
+    authenticate_acct("stellar", secret_key)
+    qr_key = create_one_time_password(secret_key)
+    if verify_user_code(input("Insert a code"), qr_key):
+        print("true")
+    else:
+        print("false")
